37e7dafbdf08aa456b1d8d523f071c2e4542ecc0
Chris Holt <chhol@microsoft.com>
2022-06-17 11:05:44 -0700

    update button stories to illustrate max call stack on gradient helper function

--- packages/web-components/src/button/button.stories.ts
+++ packages/web-components/src/button/button.stories.ts
@@ -1,4 +1,31 @@
+
+import { html } from '@microsoft/fast-element';
+import { FoundationElement } from '@microsoft/fast-foundation';
+import addons from '@storybook/addons';
+import { DOCS_RENDERED } from '@storybook/core-events';
+import { provideFluentDesignSystem } from '../fluent-design-system';
 import { fluentButton } from './index';
+
+const template = html<TestCard>`<fluent-card> Test </fluent-card>`;
+
+class TestCard extends FoundationElement {}
+
+const testCard = TestCard.compose({
+  baseName: 'test-card',
+  template,
+});
+
+provideFluentDesignSystem().register(testCard());
+
+addons.getChannel().addListener(DOCS_RENDERED, (name: string) => {
+  if (name.toLowerCase().includes('button')) {
+    document.getElementById('one')?.addEventListener('click', () => {
+      const cards = document.getElementById('cards')!;
+      const card = document.createElement('fluent-test-card') as TestCard;
+      cards.append(card);
+    });
+  }
+});
 
 export default {
   title: 'Components/Button',
@@ -19,11 +46,13 @@ export default {
 const ButtonTemplate = ({ appearance, disabled, label }) =>
   `
     <fluent-button
+      id="one"
       ${disabled ? 'disabled' : ''}
       ${appearance ? `appearance="${appearance}"` : ''}
     >
       ${label}
     </fluent-button>
+    <div id="cards"></div>
   `;
 
 export const Button = ButtonTemplate.bind({});
